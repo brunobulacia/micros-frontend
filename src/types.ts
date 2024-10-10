eff98641ed9b1dad2917a90a7fe89286636023b0
Sebastian Claure Collao <sebastian75601012@outlook.com>
2024-10-09 20:04:43 -0400

    mas tipos

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,30 @@
-
-
 interface Coords {
     lon: number,
     lat: number
 }
 
 export interface Stop {
+    id_parada: string | null;
     nombre_parada: string,
     orden_parada: number,
     coordenadas: Coords,
     token: string, //todo Hay que quitar esto cuando arreglemos la webada de las cookies
-  }
+}
+
+export interface Driver {
+    usuario: string;
+    licencia_categoria: string;
+    nombre: string;
+    apellido: string;
+    correo: string;
+    carnet: string;
+    sexo: "M" | "F";
+    telefonos: Array<string>
+}
+
+export interface RouteType {
+    duracion_estimada: string;
+    id_linea: number;
+    id_ruta: number;
+    longitud_total: string;
+}
