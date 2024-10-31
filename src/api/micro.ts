8332a04864895b2993181fc2b94789168adc6237
Joel Cuéllar <joelcuellar2311@gmail.com>
2024-10-30 21:31:31 -0400

    gogogo ciclo 2

--- src/api/micro.ts
+++ src/api/micro.ts
@@ -7,13 +7,13 @@ export interface MicroData {
   linea: string;
   token: string;
 }
-
+import { useAuthStore } from "@/store/auth";
 // api.ts
 import axios from "./axios";
 
 export const crearMicro = async (microData: MicroData) => {
   try {
-    const response = await axios.post("/micros/crear", microData);
+    const response = await axios.post("/micros/crear", { token: useAuthStore.getState().token ,microData});
     return response.data;
   } catch (error) {
     console.error("Error al crear el micro:", error);
