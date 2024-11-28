1d29dd2fa098ac94f089022e4f6bf9ac4ea54580
Sebastian Claure Collao <sebastian75601012@outlook.com>
2024-11-27 22:37:28 -0400

    Desbordamiento del mensaje

--- src/components/cards/MessageCard.tsx
+++ src/components/cards/MessageCard.tsx
@@ -9,22 +9,22 @@ export const MessageCard = ({ mensaje }: { mensaje: Mensaje }) => {
   const usuario = decodedToken.id;
 
   return (
-    <div className="w-full">
+    <div className="w-full h-auto">
       <div
-        className={`border rounded-lg p-4 mb-4 flex flex-col sm:flex-row items-center justify-between w-full md:w-1/2 lg:w-1/3 bg-slate-200 shadow-inner ${
+        className={`border rounded-lg p-4 mb-4 flex flex-col items-start w-full sm:w-3/4 md:w-1/2 lg:w-1/3 bg-slate-200 shadow-inner ${
           mensaje.emisor === usuario ? "float-right" : "float-left"
         }`}
+        style={{ wordBreak: "break-word" }} // Forzar el salto de línea en palabras largas
       >
-        <div className="flex flex-col sm:flex-row items-center w-full sm:w-auto mb-4 sm:mb-0">
-          <div className="text-center sm:text-left">
-            <h3 className="font-bold text-lg">{mensaje.emisor}</h3>
-            <p className="text-sm text-gray-500 mb-2">
-              <b>{mensaje.rol}</b>
-            </p>
-            <p className="text-sm">{mensaje.contenido}</p>
-          </div>
+        <div className="w-full">
+          <h3 className="font-bold text-lg mb-2">{mensaje.emisor}</h3>
+          <p className="text-sm text-gray-500 mb-2">
+            <b>{mensaje.rol}</b>
+          </p>
+          <p className="text-sm break-words">{mensaje.contenido}</p>
         </div>
       </div>
     </div>
   );
+  
 };
